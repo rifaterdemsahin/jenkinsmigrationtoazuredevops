ee47f83e75ffd6292eedbdde055273c4ce61af20
Erdem <rifaterdemsahin@gmail.com>
2024-09-18 21:59:19 +0100

    Update dockerrun.sh

--- code/dockerrun.sh
+++ code/dockerrun.sh
@@ -3,3 +3,9 @@ docker run \
   -p 8080:8080 \
   -v jenkins-data:/var/jenkins_home \
   jenkins/jenkins:lts
+
+docker run \
+  --name jenkins-poc \
+  -p 8080:8080 \
+  -v jenkins-data:/var/jenkins_home \
+  jenkins-python
